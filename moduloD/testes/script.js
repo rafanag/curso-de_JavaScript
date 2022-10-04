8bffbf10c0e5394e70f881acdcb7dc776219f65d
Rafael Nagel <rafanag84@gmail.com>
2022-10-04 17:39:50 -0300

    continuacao do site
    Aqui esta com uma seleção para quantidade de numeros.
    Essa seleção será tirada e ficara no maximo 18 numeros

--- moduloD/testes/script.js
+++ moduloD/testes/script.js
@@ -6,25 +6,32 @@ let corBotoesHover = 'rgb(0, 0, 255)'
 let numerosJogo = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 let res = document.getElementById('res')
 let inf = document.getElementById('informacoes')
-
+let cont = 0;
+let maxNumeros = 18
+let qtdeNumeros = document.getElementById('_selN')
 function inserirNum(valor){
-    
+   
     let num = Number(valor)
     bot = document.getElementById(`_n${num}`)
-   if(numerosJogo[valor - 1] == 0){
-     numerosJogo[num - 1] = num
-     bot.style.background = 'green'
-     bot.style.color =  'white'
-
+    
+    if(numerosJogo[valor - 1] == 0){
+    if(cont < 15) {
+       numerosJogo[num - 1] = num
+       bot.style.background = 'green'
+       bot.style.color =  'white'
+       cont++
+     }
    }else{
     numerosJogo[num - 1] = 0
     bot.style.background = 'white'
     bot.style.color =  'black'
-   }
+    cont--
    
+}
     //res.innerHTML = `O numero é ${num}`
     res.innerHTML = `Clicou no ${valor} <br>`
     res.innerHTML += numerosJogo
+    inf.innerHTML = `A contagem esta em ${cont}`
    
 }
 
@@ -48,5 +55,9 @@ function saiu(valor){
         //inf.innerHTML = `A cor do botao agora é ${(corFundoBot)}`
         botao.style.backgroundColor = corFundoBotoes
      }
+}
+
+function gerarResultados(){
+    inf.innerHTML = `A quantidade de numeros a ser jogada é ${qtdeNumeros.value}`
 }
 
